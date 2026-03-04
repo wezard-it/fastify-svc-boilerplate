fe0cd8dbf9a72008896915dedf2e26abb6e5942b
Michele Mattiello <michele.mattiello2001@gmail.com>
2026-03-04 09:19:40 +0100

    fix: 🐛 envs

--- tests/auth.seeders.ts
+++ tests/auth.seeders.ts
@@ -1,14 +1,18 @@
 import { getSupabaseAdminClient } from '../src/utils/supabase'
 
 async function cleanAuthTests() {
-    const supabase = await getSupabaseAdminClient()
-    const { data: users } = await supabase.auth.admin.listUsers()
-    for (const user of users.users) {
-        if (user.user_metadata.email.includes('test')) {
-            console.log('Deleting user: ', user.user_metadata.email)
-            await supabase.auth.admin.deleteUser(user.id)
-            await new Promise((resolve) => setTimeout(resolve, 200)) // Delay for Supabase
+    try {
+        const supabase = await getSupabaseAdminClient()
+        const { data: users } = await supabase.auth.admin.listUsers()
+        for (const user of users.users) {
+            if (user.user_metadata.email.includes('test')) {
+                console.log('Deleting user: ', user.user_metadata.email)
+                await supabase.auth.admin.deleteUser(user.id)
+                await new Promise((resolve) => setTimeout(resolve, 200)) // Delay for Supabase
+            }
         }
+    } catch (error) {
+        console.error('Error cleaning auth tests: ', error)
     }
 }
 
